956d054a05eb7e4aec974d0b155a1030d8cee6ee
bgizzl <hidekazu.nakatani@gmail.com>
2017-07-23 20:00:04 +0900

    add
    add

--- source/conf.py
+++ source/conf.py
@@ -96,7 +96,6 @@ html_theme = 'bizstyle'
 #html_theme = 'traditional'
 #html_theme = 'epub'
 
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
